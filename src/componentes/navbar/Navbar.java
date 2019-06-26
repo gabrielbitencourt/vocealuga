4297913dffa24b234cd2ac394cf820b1848bd926
Victor Augusto <victor_engenharia@poli.ufrj.br>
2019-06-26 00:00:08 -0300

    +entrega na navbar

--- src/componentes/navbar/Navbar.java
+++ src/componentes/navbar/Navbar.java
@@ -19,6 +19,10 @@ public class Navbar {
     public void toVeiculos(ActionEvent event) throws IOException {
         Navigate.to(this.getClass(), "veiculo/listagem/veiculo.listagem.fxml");
     }
+    public void toEntrega(ActionEvent event) throws IOException {
+        Navigate.to(this.getClass(), "reserva/listagem/entrega.fxml");
+    }
+    
 
     public void logout(ActionEvent event) throws IOException {
         Navigate.to(this.getClass(), "login/login.fxml");
